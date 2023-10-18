fcb8cdedd4be71974a4b5d79ba5582314f2579ae
MASADA <148013118+IWIMASADA@users.noreply.github.com>
2023-10-18 03:47:43 -0700

    new layout fixes

--- NewEntradoxWebsite/CSS/scrolling.js
+++ NewEntradoxWebsite/CSS/scrolling.js
@@ -1,17 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
     let currentPage = 1;
     const totalPages = 5;
+    let lastScrollTime = 0;
+    let isSwitching = false;
 
     window.addEventListener('wheel', function (e) {
-        if (e.deltaY > 0 && currentPage < totalPages) {
-            // Scrolling down
-            currentPage++;
-        } else if (e.deltaY < 0 && currentPage > 1) {
-            // Scrolling up
-            currentPage--;
-        }
+        const currentTime = new Date().getTime();
+        const timeDiff = currentTime - lastScrollTime;
 
-        liftPages();
+        if (timeDiff > 500 && !isSwitching) {
+            if (e.deltaY > 0 && currentPage < totalPages) {
+                // Scrolling down
+                isSwitching = true;
+                currentPage++;
+            } else if (e.deltaY < 0 && currentPage > 1) {
+                // Scrolling up
+                isSwitching = true;
+                currentPage--;
+            }
+
+            liftPages();
+            lastScrollTime = currentTime;
+
+            setTimeout(function () {
+                isSwitching = false;
+            }, 800); // Set the delay time in milliseconds
+        }
     });
 
     function liftPages() {
