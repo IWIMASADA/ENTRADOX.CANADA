3d971f02e79cfcce4791a0318ce252a6b99ef0b7
MASADA <148013118+IWIMASADA@users.noreply.github.com>
2023-10-23 21:47:43 -0700

    added scroll particles disintigration

--- NewEntradoxWebsite/CSS/sceneMain.js
+++ NewEntradoxWebsite/CSS/sceneMain.js
@@ -224,7 +224,7 @@ document.addEventListener('keydown', function(event) {
         // Check if scroll position meets a certain condition (adjust as needed)
         if (disWait === false) {
           // Trigger disintegration
-          disintegrationTriggered = true
+          disintegrationTriggered = true;
           disintegrateParticles();
     
     
@@ -247,6 +247,85 @@ document.addEventListener('keydown', function(event) {
     
 
   };
+
+
+
+  let currentPage = 1;
+  const totalPages = 5;
+  let lastScrollTime = 0;
+  let isSwitching = false;
+
+  window.addEventListener('wheel', function (e) {
+      const currentTime = new Date().getTime();
+      const timeDiff = currentTime - lastScrollTime;
+
+      if (timeDiff > 500 && !isSwitching) {
+          if (e.deltaY > 0 && currentPage < totalPages) {
+              // Scrolling down
+              isSwitching = true;
+              currentPage++;
+          } else if (e.deltaY < 0 && currentPage > 1) {
+              // Scrolling up
+              isSwitching = true;
+              currentPage--;
+          }
+
+          liftPages();
+          lastScrollTime = currentTime;
+
+          setTimeout(function () {
+              isSwitching = false;
+          }, 800); // Set the delay time in milliseconds
+      }
+  });
+
+  function liftPages() {
+
+      // Get all pages
+      const pages = document.querySelectorAll('.sectionWrapper');
+      const homePage = document.getElementById('page1');
+
+      var initialHomeTop = homePage.style.top;
+      // Set top values dynamically
+
+
+      pages.forEach((page, index) => {
+          page.style.top = `${(index - currentPage + 1) * 100}%`;
+      });
+
+      // Show the pages
+      pages.forEach(page => {
+          page.style.display = 'block';
+      });
+
+      if (homePage.style.top == '-100%' && initialHomeTop == '0%') {
+                // Check if scroll position meets a certain condition (adjust as needed)
+                if (disWait === false) {
+                  // Trigger disintegration
+                  disintegrationTriggered = true;
+                  disintegrateParticles();
+            
+            
+                  disintegrationInProgress = true;
+            
+                  
+                } else {
+            
+                }
+      }
+
+      if (homePage.style.top == '0%' && initialHomeTop == '-100%') {
+        disintegrationInProgress = false;
+        disWait = false;
+  
+  
+        requestAnimationFrame(animateDisintegration);
+  
+        particles.material = particleMaterial;
+      }
+
+
+  }
 
 
 
@@ -310,5 +389,8 @@ document.addEventListener('keydown', function(event) {
   animate();
 
 };
+
+
+
 
 
