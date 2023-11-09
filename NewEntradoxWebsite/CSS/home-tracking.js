4a88a63566e274db684c4d8a417f32220e02814f
MASADA <148013118+IWIMASADA@users.noreply.github.com>
2023-11-08 21:33:17 -0800

    finished robots page

--- NewEntradoxWebsite/CSS/home-tracking.js
+++ NewEntradoxWebsite/CSS/home-tracking.js
@@ -1,6 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     var sponsorInformationContainerMainDisplay = document.querySelector('.sponsorInformationContainerMainDisplay');
     var sponsorTiersTextContent = document.querySelector('.SponsorTiersTextContent');
+    var rd = document.querySelector('.robot-description');
+    var ro2 = document.querySelector('.robot-name-outline');
+    var ron = document.querySelector('.robot-name');
+    var ro = document.querySelector('.robot-name-outline2');
+    var abe = document.querySelector('.AboutUsTextTitle');
+    var abeo = document.querySelector('.AboutUsTextTitleOutline');
+    var abed = document.querySelector('.AboutUsTextDesc');
     var frame = document.querySelectorAll('[data-custom="homeMovingItems"]');
     frame.forEach(frameObject => {
         document.addEventListener('mousemove', (e) => {
@@ -24,6 +31,12 @@ document.addEventListener('DOMContentLoaded', function() {
         const translateY = (y - window.innerHeight / 2) * 0.013;
         sponsorInformationContainerMainDisplay.style.transform = `translate(${translateX}px, ${translateY}px)`;
         sponsorTiersTextContent.style.transform = `translate(${translateX}px, ${translateY}px)`;
+        rd.style.transform = `translate(${translateX}px, ${translateY}px)`;
+        ro2.style.transform = `translate(${translateX}px, ${translateY}px)`;
+        ron.style.transform = `translate(${translateX}px, ${translateY}px)`;
+        ro.style.transform = `translate(${translateX}px, ${translateY}px)`;
+        abe.style.transform = `translate(${translateX}px, ${translateY}px)`;
+        abeo.style.transform = `translate(${translateX}px, ${translateY}px)`;
+        abed.style.transform = `translate(${translateX}px, ${translateY}px)`;
     })
 });
-
--- NewEntradoxWebsite/CSS/robot-model-script.js
+++ NewEntradoxWebsite/CSS/robot-model-script.js
@@ -1,7 +1,6 @@
 import * as THREE from 'https://cdn.skypack.dev/three@0.133.0/build/three.module.js';
 import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.133.0/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from "https://cdn.skypack.dev/three@0.133.0/examples/jsm/controls/OrbitControls.js";
-
 const loader = new GLTFLoader();
 const canvas = document.querySelector('.robot-model-container');
 const scene = new THREE.Scene();
@@ -9,7 +8,6 @@ const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerH
 const renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 canvas.appendChild(renderer.domElement);
-
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.dampingFactor = 0.25;
@@ -17,36 +15,27 @@ controls.screenSpacePanning = false;
 controls.maxPolarAngle = Math.PI / 2;
 controls.enableZoom = false;
 controls.enablePan = false;
-
 const ambientLight = new THREE.AmbientLight(0x424242);
 scene.add(ambientLight);
-
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
 directionalLight.position.set(5, 5, 5).normalize();
 scene.add(directionalLight);
-
 const pointLight = new THREE.PointLight(0xffffff, 2, 50);
 pointLight.position.set(0, 10, 10);
 scene.add(pointLight);
-
 const hemisphereLight = new THREE.HemisphereLight(0xddeeff, 0x202020, 1);
 scene.add(hemisphereLight);
-
-camera.position.z = 2.3;
+camera.position.z = 22;
 camera.fov = 60; 
 camera.updateProjectionMatrix();
-
 let isMouseDown = false;
 let isModelLoaded = false;
-
-// Frame rate control variables
-const targetFPS = 60;
+const targetFPS = 50;
 const frameTime = 1000 / targetFPS;
 let lastTimestamp = 0;
-
 function loadModel() {
     loader.load(
-        './robotmodel/scene.gltf',
+        '1892382.glb', 
         function (gltf) {
             const loadedModel = gltf.scene;
             loadedModel.traverse((child) => {
@@ -57,28 +46,21 @@ function loadModel() {
                     }
                 }
             });
-            loadedModel.position.set(0, -loadedModel.position.y - 0.8, 0);
+            loadedModel.position.set(0, -loadedModel.position.y - 7, 0);
             scene.add(loadedModel);
             isModelLoaded = true;
             animate();
         }
     );
 }
-
-// Intersection Observer options
 const observerOptions = {
     root: null,
     rootMargin: '0px',
     threshold: 0.5,
 };
-
-// Create an Intersection Observer
 const intersectionObserver = new IntersectionObserver(handleIntersection, observerOptions);
-
-// Target element for the Intersection Observer
 const targetElement = document.querySelector('.robot-model-container');
 intersectionObserver.observe(targetElement);
-
 function handleIntersection(entries, observer) {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -87,36 +69,29 @@ function handleIntersection(entries, observer) {
         }
     });
 }
-
 document.addEventListener('mousemove', (e) => {
     const x = e.clientX;
     const y = e.clientY;
-    const translateX = (x - window.innerWidth / 2) * 0.03;
-    const translateY = (y - window.innerHeight / 2) * 0.03;
+    const translateX = (x - window.innerWidth / 2) * 0.015;
+    const translateY = (y - window.innerHeight / 2) * 0.015;
     canvas.style.transform = `translate(${translateX}px, ${translateY}px)`;
 });
-
 function animate(timestamp) {
     const elapsed = timestamp - lastTimestamp;
-
     if (elapsed > frameTime) {
         if (isMouseDown) {
             controls.update();
         } else {
-            scene.rotation.y += 0.0005;
+            scene.rotation.y -= 0.0005;
             controls.update();
             renderer.render(scene, camera);
         }
-
         lastTimestamp = timestamp - (elapsed % frameTime);
     }
-
     requestAnimationFrame(animate);
 }
-
-// Loader for initial load
 loader.load(
-    './robotmodel/scene.gltf',
+    '1892382.glb',
     function (gltf) {
         const loadedModel = gltf.scene;
         loadedModel.traverse((child) => {
@@ -127,7 +102,7 @@ loader.load(
                 }
             }
         });
-        loadedModel.position.set(0, -loadedModel.position.y - 0.8, 0);
+        loadedModel.position.set(0, -loadedModel.position.y - 7, 0);
         scene.add(loadedModel);
         isModelLoaded = true;
         animate();
